b43208e62b3d7ebc1d1f42de88eeeacd470fb40a
yesyxv <31327501+yesyxv@users.noreply.github.com>
2019-11-18 11:39:28 +0100

    Update Db.class.php

--- models/Db.class.php
+++ models/Db.class.php
@@ -7,7 +7,16 @@ class Db
     private function __construct()
     {
         try {
-            $this->_db = new PDO('mysql:host=localhost;dbname=bdbn;charset=utf8', 'root', '');
+			$env = parse_url(getenv("DATABASE_URL"));
+            #$this->_db = new PDO('mysql:host=localhost;dbname=bdbn;charset=utf8', 'root', '');
+			$this->_db = new PDO("pgsql:" . sprintf(
+				"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+				$db["host"],
+    			$db["port"],
+    			$db["user"],
+    			$db["pass"],
+    			ltrim($db["path"], "/")
+			));
             $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			$this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
         } 
