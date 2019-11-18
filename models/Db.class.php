5b6c2fb8a0461720dea01537c60d9c726f97ead3
yesyxv <31327501+yesyxv@users.noreply.github.com>
2019-11-18 11:48:31 +0100

    Update Db.class.php

--- models/Db.class.php
+++ models/Db.class.php
@@ -17,8 +17,10 @@ class Db
     			$env["pass"],
     			ltrim($env["path"], "/")
 			));
-            $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-			$this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+		
+		
+            //$this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		//	$this->_db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
         } 
 		catch (PDOException $e) {
 		    die('Erreur de connexion à la base de données : '.$e->getMessage());
